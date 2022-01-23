9bb0913fff1a8f2ec50cb33603e7a0c817b67614
mousetrap0 <james0619@naver.com>
2022-01-23 21:21:36 +0900

    세팅

---
+++ src/main/java/com/example/equinox1/Equinox1Application.java
@@ -0,0 +1,13 @@
+package com.example.equinox1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Equinox1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Equinox1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/equinox1/Equinox1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.equinox1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Equinox1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
